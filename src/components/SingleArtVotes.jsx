050a8ff78ecd17d665b364052e670de5add6c680
fdesai60 <fdesai60@gmail.com>
2025-01-05 20:48:17 +0000

    vote styling added

--- src/components/SingleArtVotes.jsx
+++ src/components/SingleArtVotes.jsx
@@ -3,13 +3,15 @@ import { patchVote } from "../api"
 import Error from "./Error"
 import styles from "../css/SingleArtVotes.module.css"
 
+
+
 const SingleArtVotes = ({votes, setVotes,singleArticle}) => {
     const [hasVoted,setHasVoted]= useState(false)
     const [error,setError]=useState(null)
 
 
 
-  
+   
     const handleVoteInc=()=>{
      
         setVotes(currCount=>currCount+1)
@@ -46,15 +48,18 @@ const SingleArtVotes = ({votes, setVotes,singleArticle}) => {
     return ( 
         <>
             {error&& <Error error={error}/>}
+            <h3>Current vote count: <span className={styles.num}>{votes}</span></h3>
+    
             <div className={styles.voteContainer}>
-            <button disabled={hasVoted}
-                    onClick={handleVoteInc}>+
-            </button>
-            <h3>{votes}</h3>
-            <button disabled={hasVoted}
-                    onClick={handleVoteDec}>-
+                <button disabled={hasVoted}
+                        onClick={handleVoteInc}>⬆
+                </button>
+                
+                <button disabled={hasVoted}
+                        onClick={handleVoteDec}>⬇
                 </button>
             </div>
+            <p className={styles.voteMsg}>{hasVoted&& "Thanks for voting!"}</p>
         
           </>
      );
